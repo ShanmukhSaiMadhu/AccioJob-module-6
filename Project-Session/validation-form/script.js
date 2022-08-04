2f3c3538119a0ca5643bea4778e034ee0c92d45a
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-08-05 00:01:36 +0530

    Array Methods

--- Project-Session/validation-form/script.js
+++ Project-Session/validation-form/script.js
@@ -8,7 +8,7 @@ function validate () {
     let zipInput = document.getElementById('zip').value
     let tnCInput = document.getElementById('tnC').checked
 
-    if(firstNameInput.length >= 1) {
+    if(firstNameInput) {
         document.getElementById('first-name-valid').style.display = 'block'
         document.getElementById('first-name-invalid').style.display = 'none'
     }else {
@@ -16,7 +16,7 @@ function validate () {
         document.getElementById('first-name-valid').style.display = 'none'
     }
 
-    if(lastNameInput.length >= 1) {
+    if(lastNameInput) {
         document.getElementById('last-name-valid').style.display = 'block'
         document.getElementById('last-name-invalid').style.display = 'none'
     }else {
@@ -24,7 +24,7 @@ function validate () {
         document.getElementById('last-name-valid').style.display = 'none'
     }
 
-    if((/\S+@\S+\.\S+/).test(emailInput)) {
+    if(emailInput && emailInput.includes('@') && emailInput.includes('.') && emailInput.lastIndexOf('.') <= emailInput.length - 3 && emailInput.indexOf('@') != 0) {
         document.getElementById('email-valid').style.display = 'block'
         document.getElementById('email-invalid').style.display = 'none'
     }else {
@@ -32,7 +32,7 @@ function validate () {
         document.getElementById('email-valid').style.display = 'none'
     }
 
-    if(cityInput.length >= 1) {
+    if(cityInput.length >= 3) {
         document.getElementById('city-valid').style.display = 'block'
         document.getElementById('city-invalid').style.display = 'none'
     }else {
@@ -40,7 +40,7 @@ function validate () {
         document.getElementById('city-valid').style.display = 'none'
     }
 
-    if(zipInput.length >= 1) {
+    if(zipInput) {
         document.getElementById('zip-valid').style.display = 'block'
         document.getElementById('zip-invalid').style.display = 'none'
     }else {

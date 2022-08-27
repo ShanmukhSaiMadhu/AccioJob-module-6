42a005ffc34d89a898f2be193e76459d461a1315
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-08-27 12:41:11 +0530

    Dynamic tracking done in V2

--- Project-Session/OrderStatusManagementV2/js/script.js
+++ Project-Session/OrderStatusManagementV2/js/script.js
@@ -18,17 +18,39 @@ const displayTracking = () => {
     homeConatiner.style.display = 'none'
 }
 
+let currentOrderNumber = 1
+
 const executeOrder = async () => {
 
-    let orderId = document.getElementById('order-id-input').value
+        let orderId = createOrderId()
 
-    if(document.getElementById('order-id-input').value == '') {
-        document.getElementById('alert').style.display = 'block'
-    }else {
-        document.getElementById('alert').style.display = 'none'
-        document.getElementById('order-id-input').value = ''
+        let item1 = document.getElementById('item-1').value
+        let price1 = document.getElementById('price-1').value
+        let item2 = document.getElementById('item-2').value
+        let price2 = document.getElementById('price-2').value
+        let item3 = document.getElementById('item-3').value
+        let price3 = document.getElementById('price-3').value
 
-        createOrderCard(orderId)
+        resetFields()
+
+        let billDetails = [
+            {
+                item: item1,
+                price: Number(price1),
+            },
+            {
+                item: item2,
+                price: Number(price2),
+            },
+            {
+                item: item3,
+                price: Number(price3),
+            }
+        ]
+
+        createOrderCard(orderId, billDetails)
+
+        currentOrderNumber++
 
         document.getElementById(orderId).innerText = 'Order Placed'
 
@@ -46,7 +68,42 @@ const executeOrder = async () => {
             document.getElementById(orderId).innerText = err
             console.log(err)
         }
+
+        
     }
+
+    const resetFields = () => {
+        // document.getElementById('order-id-input').value = ''
+        document.getElementById('item-1').value = ''
+        document.getElementById('price-1').value = ''
+        document.getElementById('item-2').value = ''
+        document.getElementById('price-2').value = ''
+        document.getElementById('item-3').value = ''
+        document.getElementById('price-3').value = ''
+    }
+
+    
+    const createOrderId = () => {
+        let todaysDate = new Date()
+
+        let temp = `MP${todaysDate.getFullYear()}${todaysDate.getMonth() + 1}${todaysDate.getDate()}`
+
+        let numOfPreceedingZeros = 3
+        if(currentOrderNumber >= 10) {
+            numOfPreceedingZeros = 2
+        } else if (currentOrderNumber >= 100) {
+            numOfPreceedingZeros = 1
+        } else if (currentOrderNumber >= 1000) {
+            numOfPreceedingZeros = 0
+        }
+
+        while(numOfPreceedingZeros--) {
+            temp = temp + '0'
+        }
+
+        return `${temp}${currentOrderNumber}`
+    }
+    
     // chefReceivedOrder(orderId)
     // .then(pizzaSauceAdded)
     // .then(firstLayerOfCheeseAdded)
@@ -59,5 +116,5 @@ const executeOrder = async () => {
     //     document.getElementById(orderId).innerText = 'The order is ready and handed over to the Zomato delivery guy!'
     // } )
     // .catch((err) => console.log(err))
-}
+
 
